06cc820dea72d354bc07518a7b10923ee3dff977
seulgi9256 <sgij56@gmail.com>
2023-11-29 09:32:44 +0900

    2023.11.29

---
+++ start-app/src/App.js
@@ -0,0 +1,15 @@
+// 파일: App.js
+import React from 'react';
+
+// JSX를 사용한 컴포넌트 정의
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, JSX World!</h1>
+      <p>JSX를 사용한 React 컴포넌트</p>
+    </div>
+  );
+};
+
+// 모듈 export
+export default App;
